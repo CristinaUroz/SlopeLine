3b7713019cd4469eba32287ea03aa36784a57295
Cristina <cristina5696@gmail.com>
2018-05-26 21:18:09 +0200

    Processament 5x5 de la imatge

--- app/src/main/java/uroz/cristina/slopeline/SelectFileActivity.java
+++ app/src/main/java/uroz/cristina/slopeline/SelectFileActivity.java
@@ -3,16 +3,15 @@ package uroz.cristina.slopeline;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.net.Uri;
 
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
@@ -24,27 +23,27 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Calendar;
+import java.util.Map;
+import java.util.TreeMap;
 
-//TODO: Arreglar ProgressBar
-//TODO: Preferencies per defecte
-
-
-public class SelectFileActivity extends AppCompatActivity  /*implements View.OnTouchListener*/ {
+public class SelectFileActivity extends AppCompatActivity{
 
     private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
@@ -55,9 +54,8 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
     private String fileName;
     private File dir;
     private String image_dir = "/SlopeLine/data/";
-    private static boolean sOpenCVAvailable = true;
     public static final String TAG = SelectFileActivity.class.getSimpleName();
-    private int pix_max = 1600;
+    private int pix_max = 1200;
     private boolean guardada = false;
     private Bitmap bm;
     private int num;
@@ -66,10 +64,24 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
     private int W;
     private int Ho;
     private int Wo;
+    private int valor_barra=50;
+    private int color_chroma = Color.parseColor("#7c8767");
+    private int color_white = Color.parseColor("#ffffff");
+    private int color_blue = Color.parseColor("#0000ff");
+    private int color_black = Color.parseColor("#000000");
+    private SeekBar barra_chroma;
+    private int[][] mat;
+    private int[][] mat_new;
+    private String[] binar;
+    private Map<String, String> replace_map;
+    private int total_imatges=852;
+    private Bitmap bm2;
+    int mPhotoWidth;
+    int mPhotoHeight;
 
-    private ProgressBar mProgressBar;
-    private int mProgressStatus = 0;
-    private Handler mHandler = new Handler();
+
+    public SelectFileActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,12 +110,47 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
         ima = (ImageView) findViewById(R.id.image_Map);
         next = (ImageView) findViewById(R.id.image_Next);
         text_ima = (TextView) findViewById(R.id.text_SelectPick);
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        barra_chroma = (SeekBar) findViewById(R.id.tolerance_bar);
+
+        Create_map();
+
+        // Configuracio de la barra
+        barra_chroma.setMax(100);
+        barra_chroma.setProgress(50);
+        if (getIntent() != null && getIntent().getExtras() != null) {
+            barra_chroma.setProgress(valor_barra);
+        }
 
 
         // Fem invisible el botó next
         next.setVisibility(View.INVISIBLE);
-        mProgressBar.setVisibility(View.INVISIBLE);
+        barra_chroma.setVisibility(View.INVISIBLE);
+
+        barra_chroma.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                valor_barra = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Per fer que no es pugui tornar a premer mentre sesta executant
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                // Mostra el valor de la barra
+                Toast.makeText(SelectFileActivity.this, String.valueOf(valor_barra), Toast.LENGTH_SHORT).show();
+
+                // Quan s'aixeca el click s'aplica l'efecte
+               change_Color();
+
+            }
+        });
+
+
 
         // Posem a les preferencies els valors per defecte
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
@@ -122,41 +169,13 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
             @Override
             public void onClick(View view) {
                 next.setVisibility(View.INVISIBLE);
-                mProgressBar.setProgress(0);
+                barra_chroma.setVisibility(View.INVISIBLE);
                 guardarImatgeInicial();
-                start();
+                Processat();
+               // start();
             }
         });
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Mirem si l'openCV està disponible cada vegada que l'app es torna a carregar
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mBaseLoaderCallback);
-    }
-
-    private BaseLoaderCallback mBaseLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                // OpenCV disponlibre
-                case LoaderCallbackInterface.SUCCESS:
-                    sOpenCVAvailable = true;
-                    Log.i(TAG, "OpenCV carregat correctament");
-                    break;
-                // Casos que poden donar error
-                case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
-                case LoaderCallbackInterface.INIT_FAILED:
-                case LoaderCallbackInterface.INSTALL_CANCELED:
-                case LoaderCallbackInterface.MARKET_ERROR:
-                    sOpenCVAvailable = false;
-                    Log.i(TAG, "Error al carregar OpenCV - codi = " + status);
-                default:
-                    super.onManagerConnected(status);
-            }
-        }
-    };
 
     // Es guarda l'uri de la imatge escollida i es fa el display a l'image view corresponent
     // Es posa visible el boto next/play
@@ -171,13 +190,13 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
                         try{
                         bm=MediaStore.Images.Media.getBitmap(this.getContentResolver(), ima_uri);
                             Resize();
-                            ima.setImageBitmap(bm);
+                            //ima.setImageBitmap(bm);
                             text_ima.setVisibility(View.INVISIBLE);
                             guardada=false;
                             next.setVisibility(View.VISIBLE);
-                            mProgressBar.setVisibility(View.VISIBLE);
-                            mProgressBar.setProgress(0);
+                            barra_chroma.setVisibility(View.VISIBLE);
                             CG='G';
+                            change_Color();
                     } catch (IOException e) {
                         e.printStackTrace();
                         return;
@@ -190,14 +209,14 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
                 if (resultCode == RESULT_OK) {
                     bm = BitmapFactory.decodeFile(fileName);
                     Resize();
-                    ima.setImageBitmap(bm);
+                    //ima.setImageBitmap(bm);
                     text_ima.setVisibility(View.INVISIBLE);
                     guardada=false;
                     //bMap.recycle();
                     next.setVisibility(View.VISIBLE);
-                    mProgressBar.setVisibility(View.VISIBLE);
-                    mProgressBar.setProgress(0);
+                    barra_chroma.setVisibility(View.VISIBLE);
                     CG='C';
+                    change_Color();
                 }
                 break;
         }
@@ -310,7 +329,7 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
         switch (id) {
             case R.id.action_settings:
                 Intent intent = new Intent(this, SettingsActivity.class);
-                startActivity(intent);
+                //startActivity(intent);
                 break;
             case R.id.info:
                 AlertDialog.Builder builder = new AlertDialog.Builder(SelectFileActivity.this);
@@ -337,96 +356,6 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
 
 
         return super.onOptionsItemSelected(item);
-    }
-
-    // Posa en marxa l'OpenCVRunnable
-    private void start() {
-        // Comprovació de que l'openCV esta disponible
-        if (!sOpenCVAvailable) {
-            // Si no està disponible es notifica a l'usuari i es torna a intentar
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(SelectFileActivity.this);
-            builder.setTitle(R.string.Opencvnotavailalbe);
-            builder.setMessage(R.string.Retry);
-            builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    start();
-                }
-            });
-            builder.create().show();
-
-        }
-        // OpenCV esta disponible i s'ha carregat correctament
-        // S'Agafen les preferències de l'usuari
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        final int blurKernelSize = Integer.parseInt(preferences.getString(getString(R.string.preference_key_blur_kernel),
-                getString(R.string.preference_default_value_blur_kernel)));
-        final boolean useOtsu = preferences.getBoolean(getString(R.string.preference_key_canny_otsu), true);
-        final int cannyMinThreshold = Integer.parseInt(preferences.getString(getString(R.string.preference_key_canny_threshold_min),
-                getString(R.string.preference_default_value_canny_threshold_min)));
-        final int cannyMaxThreshold = Integer.parseInt(preferences.getString(getString(R.string.preference_key_canny_threshold_max),
-                getString(R.string.preference_default_value_canny_threshold_max)));
-        final int contourType = Integer.parseInt(preferences.getString(getString(R.string.preference_key_contours_type),
-                getString(R.string.preference_entry_value_contours_type_external)));
-        final int shapeIgnoreSize = Integer.parseInt(preferences.getString(getString(R.string.preference_key_contours_ignore_area_size),
-                getString(R.string.preference_default_contours_ignore_area_size)));
-        final boolean shapreIgnoreConcave = preferences.getBoolean(getString(R.string.preference_key_contours_ignore_concave), true);
-        final boolean drawFilteredShapes = preferences.getBoolean(getString(R.string.preference_key_contours_draw_filtered_shapes), true);
-
-        // S'inicia l'OpenCV runnable
-        final OpenCvRunnable cvRunnable = new OpenCvRunnable((Environment.getExternalStorageDirectory() + image_dir + "_" + num + "_1_originalImage" + ".png"),
-                blurKernelSize,
-                cannyMinThreshold,
-                cannyMaxThreshold,
-                useOtsu,
-                contourType,
-                shapeIgnoreSize,
-                shapreIgnoreConcave,
-                drawFilteredShapes,
-                getApplicationContext());
-        // Les accions de l'openCV requereixen CPU
-        // Posarem les acctions en marxa apart per a no colapsar el main
-        Thread cvThread = new Thread(cvRunnable);
-
-        bm = null;
-
-
-        Thread pbThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (mProgressStatus < 100) {
-                    mProgressStatus =cvRunnable.getStatus();
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mProgressBar.setProgress(mProgressStatus);
-                        }
-                    });
-                }
-            }
-        });
-
-        cvThread.start();
-        pbThread.start();
-
-        while (bm==null){
-            bm=cvRunnable.getFinalbitmap();
-        }
-        ima.setImageBitmap(bm);
-        String fullPath = Environment.getExternalStorageDirectory() + image_dir;
-
-        num=0;
-        // Comprova si el directori existeix per crear-lo en cas de que no sigui així
-        try{
-        File dir = new File(fullPath);
-        File file = new File(fullPath+ "Settings.txt");
-        String text_settings=cvRunnable.toString()+";"+CG+";"+Wo+"x"+Ho+ ";"+W+"x"+H;
-        FileOutputStream fileOutputStream = new FileOutputStream(file,true);
-        fileOutputStream.write((text_settings + System.getProperty("line.separator")).getBytes());}
-        catch (IOException e) {
-        }
-
     }
 
     public void Resize() {
@@ -524,4 +453,127 @@ public class SelectFileActivity extends AppCompatActivity  /*implements View.OnT
 
     }
 
+    public void change_Color() {
+        bm2 = convertToMutable(bm);
+        mPhotoWidth = bm2.getWidth();
+        mPhotoHeight = bm2.getHeight();
+        int[] pix = new int[mPhotoWidth * mPhotoHeight];
+        int index, r, g, b, yy, R, G, B, Y;
+        R = (color_chroma >> 16) & 0xff;
+        G = (color_chroma >> 8) & 0xff;
+        B = color_chroma & 0xff;
+        Y = (30 * R + 59 * G + 11 * B) / 100;
+        mat=new int[mPhotoWidth][mPhotoHeight];
+        mat_new=new int[mPhotoWidth][mPhotoHeight];
+        int tol=valor_barra;
+        bm2.getPixels(pix, 0, mPhotoWidth, 0, 0, mPhotoWidth, mPhotoHeight);
+        for (int y = 0; y < mPhotoHeight; y++) {
+            for (int x = 0; x < mPhotoWidth; x++) {
+                index = y * mPhotoWidth + x;
+                r = (pix[index] >> 16) & 0xff;
+                g = (pix[index] >> 8) & 0xff;
+                b = pix[index] & 0xff;
+                yy = (30 * r + 59 * g + 11 * b) / 100;
+                //Mirem si els valors del color es troben dins dels parametres de tolerancia
+                if (!(Y + tol >= yy && Y - tol <= yy &&
+                        R + tol >= r && R - tol <= r &&
+                        B + tol >= b && B - tol <= b &&
+                        G + tol >= g && G - tol <= g
+                )) {
+                    //pix[index] = getResources().getColor(transparent);
+                    pix[index] = color_white;
+                    mat[x][y]=0;
+                    mat_new[x][y]=-1;
+
+                }
+                else{
+                    pix[index] = color_black;
+                    mat[x][y]=1;
+                    mat_new[x][y]=-1;
+                }
+            }
+        }
+
+        bm2.setPixels(pix, 0, mPhotoWidth, 0, 0, mPhotoWidth, mPhotoHeight);
+        // Fem visible el nou bitmap
+        ima.setImageBitmap(bm2);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
+
+    public static Bitmap convertToMutable(Bitmap imgIn) {
+        try {
+            // Fitxer temporal de treball que conte els bits de la imatge (no es una imatge)
+            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "temp.tmp");
+
+            // Es crea un RandomAccessFile
+            RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+
+            // Ample i alt del bitmap
+            int width = imgIn.getWidth();
+            int height = imgIn.getHeight();
+            Bitmap.Config type = imgIn.getConfig();
+
+            // Copy the byte to the file
+            //Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
+            FileChannel channel = randomAccessFile.getChannel();
+            MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes() * height);
+            imgIn.copyPixelsToBuffer(map);
+            //imgIn.recycle();
+            System.gc();
+
+            // Es crea el bitmap que es podra editar i s'hi carrega l'anterior
+            imgIn = Bitmap.createBitmap(width, height, type);
+            map.position(0);
+            // load it back from temporary
+            imgIn.copyPixelsFromBuffer(map);
+            // close the temporary file and channel , then delete that also
+            channel.close();
+            randomAccessFile.close();
+            // delete the temp file
+            file.delete();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imgIn;
+    }
+
+    private void Create_map() {
+        binar = getResources().getStringArray(R.array.binaritzation);
+        replace_map = new TreeMap<String, String>();
+        for (int i = 0; i < total_imatges; i++) {
+            String q = binar[i];
+            String[] parts = q.split(";");
+            replace_map.put(parts[0],parts[1]);
+        }
+    }
+
+    private void Processat(){
+        for (int y = 0; y < mPhotoHeight; y++) {
+            for (int x = 0; x < mPhotoWidth; x++) {
+                String t="";
+                for (int yy=0;yy<5;yy++){
+                    int auxy=yy+y;
+                    for (int xx=0;xx<5;xx++){
+                        int auxx=xx+x;
+                        if (mat[0].length>auxy & mat.length>auxx){
+                        t=t+Integer.toString(mat[auxx][auxy]);}
+                    }
+                }
+                String t_n= replace_map.get(t);
+                if (t_n!=null){
+                    char[] tArray= t_n.toCharArray();
+                    for (int y2=0;y2<5;y2++){
+                        int auxy2=y2+y;
+                        for (int x2=0;x2<5;x2++){
+                            int auxx2=x2+x;
+                            bm2.setPixel(auxx2,auxy2,color_blue);
+                            mat_new[auxx2][auxy2]=Character.getNumericValue(tArray[y2*5+x2]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
